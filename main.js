60e9aaacd761fc9a3a362abed48af5ec260a3bb4
Anurag Kumar <65001074+anuragkumar19@users.noreply.github.com>
2020-07-12 21:54:40 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log('connection')
+
+let thm = Qs.parse(location.search, {
+  ignoreQueryPrefix: true
+});
+
+console.log(thm.t)
+
+let linktag = `<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.0/${thm.t}/bootstrap.min.css">`
+console.log(linktag)
+
+$('head').append(linktag)
